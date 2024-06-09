f3572243d61d2d67372a66d70ba6238b9011f95b
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2024-06-10 02:07:47 +0700

    feat: misc

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import nocache from 'nocache';
 import 'dotenv/config';
 
 import router from '~/routes/index';
@@ -10,12 +12,14 @@ const port = process.env.PORT ?? 3000; // Port number
 
 // Express configuration
 app.use(cors()); // Enable CORS
+app.use(nocache())
+app.use(morgan('short'));
 app.use(express.json()); // Enable JSON body parser
 
 // Start Express server
 app.listen(port, () => {
   // Callback function when server is successfully started
-  console.log(`Server started at http://localhost:${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 // Use routes
--- src/handlers/sample.ts
+++ src/handlers/sample.ts
@@ -3,9 +3,19 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-export const sampleHandler = async (req: Request, res: Response)=> {
-  res.send("Hello World from Sample Routes!");
+export const sampleHandler = async (req: Request, res: Response) => {
+  res.send({
+    status: "OK",
+    message: "Hello world!",
+  });
 };
+
+export const sampleWithNameHandler = async (req: Request, res: Response) => {
+  res.send({
+    status: "OK",
+    message: `Hello, ${req.params.name}`,
+  });
+}
 
 export const pingHandler = async (req: Request, res: Response) => {
   res.send({
