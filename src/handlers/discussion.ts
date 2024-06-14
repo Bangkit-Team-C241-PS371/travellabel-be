4287b0b776cfa8d48dd567d321c54c71bb1e86e7
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2024-06-14 15:16:49 +0700

    feat: discussion and discussion reply API implementation

---
+++ src/handlers/discussion.ts
@@ -0,0 +1,148 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { sendErrorResponse } from "~/errors/responseError";
+
+const db = new PrismaClient();
+
+export const createDiscussionHandler = async (req: Request, res: Response) => {
+  const { locationId, title, content } = req.body;
+  const { id: creatorId } = req.user;
+
+  if (!locationId || !title || !content) {
+    return sendErrorResponse(res, 400, "Bad request data: missing fields");
+  }
+
+  const location = await db.location.findUnique({
+    where: {
+      id: locationId,
+    },
+  });
+
+  if (!location) {
+    return sendErrorResponse(
+      res,
+      404,
+      `Location with ID ${locationId} not found`
+    );
+  }
+
+  try {
+    const discussion = await db.discussion.create({
+      data: {
+        location: {
+          connect: {
+            id: locationId
+          },
+        },
+        creator: {
+          connect: {
+            id: creatorId
+          },
+        },
+        title,
+        content,
+      }
+    });
+
+    return res.status(201).send({
+      status: "OK",
+      message: "Discussion created successfully",
+      discussion,
+    });
+  } catch (e) {
+    return sendErrorResponse(res, 500, "Error while creating discussion");
+  }
+};
+
+export const createDiscussionReplyHandler = async (req: Request, res: Response) => {
+  const { discussionId } = req.params;
+  const { content } = req.body;
+  const { id: creatorId } = req.user;
+
+  if (!content) {
+    return sendErrorResponse(
+      res,
+      400,
+      "Bad request: missing content"
+    );
+  }
+
+  const discussion = await db.discussion.findUnique({
+    where: {
+      id: discussionId,
+    },
+  });
+
+  if (!discussion) {
+    return sendErrorResponse(
+      res,
+      404,
+      `Discussion with ID ${discussionId} not found`
+    );
+  }
+
+  try {
+    const reply = await db.discussionReply.create({
+      data: {
+        creator: {
+          connect: {
+            id: creatorId,
+          }
+        },
+        discussion: {
+          connect: {
+            id: discussionId,
+          }
+        },
+        content,
+      }
+    });
+
+    return res.status(201).send({
+      status: "OK",
+      message: "Reply created successfully",
+      reply,
+    });
+  } catch (e) {
+    return sendErrorResponse(res, 500, "Error while creating discussion reply");
+  }
+}
+
+export const getDiscussionRepliesHandler = async (req: Request, res: Response) => {
+  const { discussionId } = req.params;
+
+  const discussion = await db.discussion.findUnique({
+    where: {
+      id: discussionId,
+    },
+  });
+
+  if (!discussion) {
+    return sendErrorResponse(
+      res,
+      404,
+      `Discussion with ID ${discussionId} not found`
+    );
+  }
+
+  const replies = await db.discussionReply.findMany({
+    where: {
+      discussionId
+    },
+    include: {
+      creator: {
+        select: {
+          id: true,
+          username: true,
+          email: true
+        }
+      }
+    }
+  });
+
+  return res.send({
+    status: "OK",
+    message: "Replies fetched successfully",
+    replies,
+  });
+}
---
+++ src/routes/discussion.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createDiscussionHandler, createDiscussionReplyHandler, getDiscussionRepliesHandler } from "~/handlers/discussion";
+
+const discussionRouter = Router();
+
+discussionRouter.post("/", createDiscussionHandler);
+discussionRouter.post("/:discussionId", createDiscussionReplyHandler);
+discussionRouter.get("/:discussionId", getDiscussionRepliesHandler);
+
+export default discussionRouter;
