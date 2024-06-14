bb17c12585f678946ba7922f7a21184eace7be5a
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2024-06-14 16:18:11 +0700

    feat: jwt added backdated nbf claim

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { db } from "~/utils/db";
 
 export const checkEmailorUsername = async (identifier: string) => {
@@ -9,32 +9,35 @@ export const checkEmailorUsername = async (identifier: string) => {
       where: {
         email: identifier,
       },
-    })
+    });
   } else {
     // Identifier is a username
     return await db.user.findUnique({
       where: {
         username: identifier,
       },
-    })
+    });
   }
-}
+};
 
 export const createJwtToken = (
   payload: object,
   secret: string,
   expiresIn: string
 ) => {
-  return jwt.sign(payload, secret, { expiresIn })
-}
+  return jwt.sign(payload, secret, {
+    expiresIn,
+    notBefore: "-1m",
+  }); // nbf -1 minute to allow for time skew
+};
 
 export const hashPassword = async (password: string) => {
-  return await bcrypt.hash(password, 10)
-}
+  return await bcrypt.hash(password, 10);
+};
 
 export const verifyPassword = async (
   password: string,
   hashedPassword: string
 ) => {
-  return await bcrypt.compare(password, hashedPassword)
-}
+  return await bcrypt.compare(password, hashedPassword);
+};
