1170092e8b399e106da88e9497dca292c0b2cb22
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2024-06-14 22:20:49 +0700

    fix: alter prisma logging behavior on dev

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
 export const db = new PrismaClient({
-  log: process.env.NODE_ENV === "production" ? ["warn", "error"] : ["query"],
+  log: process.env.NODE_ENV === "production" ? ["warn", "error"] : ["query", "warn", "error"],
 });
