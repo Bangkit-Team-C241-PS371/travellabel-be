361335710dadd018dedd84281058c642def63953
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2024-06-19 22:28:56 +0700

    feat: merging of prediction and location data

--- src/handlers/model.ts
+++ src/handlers/model.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import axios, { AxiosError } from "axios";
 import { sendErrorResponse } from "~/errors/responseError";
 import { StatusCodes } from "http-status-codes";
+import { db } from "~/utils/db";
 
 const PREDICTIONS_ENDPOINT = process.env.RECSYS_COLLAB_ENDPOINT!;
 
@@ -25,12 +26,27 @@ export const getRecommendationHandler = async (req: Request, res: Response) => {
       }
     );
 
-    const predictions = predResponse.data.predictions;
+    const predictions: string[] = predResponse.data.predictions;
+    // correlate predictions (label string) to the corresponding location data in db
+    const predictionLocations = await db.location.findMany({
+      where: {
+        label: {
+          in: predictions
+        }
+      },
+    });
+
+    // sort locations data according to the initial predictions data that has been sorted by score from the model
+    predictionLocations.sort((a, b) => {
+      const indexA = predictions.indexOf(a.label);
+      const indexB = predictions.indexOf(b.label);
+      return indexA - indexB;
+    });
 
     res.send({
       status: "OK",
       message: "Predictions inferred successfully",
-      predictions,
+      recommendations: predictionLocations,
     });
   } catch (error: any) {
     if (error instanceof AxiosError) {
@@ -48,6 +64,5 @@ export const getRecommendationHandler = async (req: Request, res: Response) => {
         error: `Failed to fetch predictions`,
       });
     }
-
   }
 };
